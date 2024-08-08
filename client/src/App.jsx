716eb6a2a91d91c47ee18916e3640627175ac2ad
tedschu <ted.schuster@gmail.com>
2024-08-08 10:12:52 -0500

    updating client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,158 @@
+import { useEffect, useState } from "react";
+import { Route, Router, Routes, useNavigate } from "react-router-dom";
+import React from "react";
+import Game from "./pages/Game";
+
+function App() {
+  const storedToken = localStorage.getItem("token");
+
+  const [isLoggedIn, setIsLoggedIn] = useState(!!storedToken);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    birth_year: "",
+    email: "",
+    username: "",
+    password: "",
+    security_question_1: "",
+    security_answer_1: "",
+    security_question_2: "",
+    security_answer_2: "",
+  });
+  // *********** UPDATE WITH VALUES FROM DB TABLE
+  const [userScore, setUserScore] = useState({
+    score: 0,
+  });
+  const [userBadges, setUserBadges] = useState({
+    bernese: false,
+    boxer: false,
+    cat: false,
+    chihuahua: false,
+    golden: false,
+    husky: false,
+    goldendoodle_trophy: false,
+  });
+  const [token, setToken] = useState(storedToken || "");
+
+  const navigate = useNavigate();
+
+  // Verifies that a user is loggedIn (checks for token)
+  // IF token exists: update setters (isLoggedIn, badges, userscore, userId)
+  // IF token doesn't exist, navigate to /login
+
+  useEffect(() => {
+    if (storedToken) {
+      setIsLoggedIn(true);
+      setToken(storedToken);
+      //setUserId(localStorage.getItem("userId"));
+
+      // Gets all relevant user data (user info, scores, badges) and stores in state for usage throughout the app
+      const getUserData = async () => {
+        try {
+          const response = await fetch("/api/users/me", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${storedToken}`,
+            },
+          });
+
+          const data = await response.json();
+
+          // SET ALL STATE VALUES HERE (SCORES, BADGES, USER INFO, ETC.)
+          if (response.ok) {
+            setUserInfo({
+              id: data.id,
+              username: data.username,
+              name: data.name,
+              birth_year: data.birth_year,
+              email: data.email,
+              security_question_1: data.security_question_1,
+              security_answer_1: data.security_answer_1,
+              security_question_2: data.security_question_2,
+              security_answer_2: data.security_answer_2,
+            });
+            setUserScore(data.score);
+            setUserBadges(data.badge);
+            setTotalScore(
+              parseInt(data.score.addition_score) +
+                parseInt(data.score.subtraction_score) +
+                parseInt(data.score.multiplication_score) +
+                parseInt(data.score.division_score)
+            );
+          }
+          // ADDED TO HANDLE CASE WHERE API CALL IS BAD OR HASN'T COME BACK
+          else if (!response.ok) {
+            navigate("/welcome");
+          }
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      };
+
+      getUserData();
+    }
+  }, [isLoggedIn]); // SET TO ISLOGGEDIN TO ENSURE RELOAD POST LOGIN AND REGISTRATION
+
+  return (
+    <>
+      {/* <Nav isLoggedIn={isLoggedIn} userInfo={userInfo} /> */}
+
+      <Routes>
+        <Route
+          index
+          element={
+            <Game
+              isLoggedIn={isLoggedIn}
+              userInfo={userInfo}
+              userScore={userScore}
+              setUserScore={setUserScore}
+              userBadges={userBadges}
+              setUserBadges={setUserBadges}
+              totalScore={totalScore}
+              setTotalScore={setTotalScore}
+            />
+          }
+        />
+        <Route
+          path="/me"
+          element={
+            <Me
+              isLoggedIn={isLoggedIn}
+              userInfo={userInfo}
+              userScore={userScore}
+              totalScore={totalScore}
+              userBadges={userBadges}
+              setIsLoggedIn={setIsLoggedIn}
+            />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Register
+              setIsLoggedIn={setIsLoggedIn}
+              isLoggedIn={isLoggedIn}
+              userInfo={userInfo}
+              setUserInfo={setUserInfo}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Login
+              setIsLoggedIn={setIsLoggedIn}
+              isLoggedIn={isLoggedIn}
+              userInfo={userInfo}
+              setUserInfo={setUserInfo}
+            />
+          }
+        />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/about" element={<About isLoggedIn={isLoggedIn} />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Game.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import ScoreBar from "../components/ScoreBar";
+import GamePlay from "../components/GamePlay";
+import Nav from "../components/Nav";
+import BadgeModal from "../components/BadgeModal";
+
+function Game({
+  isLoggedIn,
+  loginForm,
+  userScore,
+  setUserScore,
+  userBadges,
+  setUserBadges,
+  userInfo,
+  totalScore,
+  setTotalScore,
+}) {
+  const [sliderValue, setSliderValue] = useState(1);
+  const [gotRight, setGotRight] = useState(false);
+  const [gotWrong, setGotWrong] = useState(false);
+
+  // state for modal that opens when a new badge is won
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalBadge, setModalBadge] = useState("");
+
+  const navigate = useNavigate();
+
+  // If a user is not signed in (no token) they are redirected to the login page.
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      navigate("/welcome");
+    }
+  }, []);
+
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <>
+      <Nav isLoggedIn={isLoggedIn} userInfo={userInfo} />
+
+      <div className="mainContainer">
+        <ScoreBar
+          isLoggedIn={isLoggedIn}
+          loginForm={loginForm}
+          userScore={userScore}
+          setUserScore={setUserScore}
+          userBadges={userBadges}
+          setUserBadges={setUserBadges}
+          sliderValue={sliderValue}
+          gameSelector={gameSelector}
+          userInfo={userInfo}
+          totalScore={totalScore}
+          setTotalScore={setTotalScore}
+        />
+
+        <GameSelector
+          setGameSelector={setGameSelector}
+          setGotRight={setGotRight}
+          setGotWrong={setGotWrong}
+        />
+
+        <Slider
+          setSliderValue={setSliderValue}
+          sliderValue={sliderValue}
+          setGotRight={setGotRight}
+          setGotWrong={setGotWrong}
+        />
+
+        <GamePlay
+          sliderValue={sliderValue}
+          gameSelector={gameSelector}
+          userScore={userScore}
+          setUserScore={setUserScore}
+          userInfo={userInfo}
+          setTotalScore={setTotalScore}
+          totalScore={totalScore}
+          gotRight={gotRight}
+          gotWrong={gotWrong}
+          setGotRight={setGotRight}
+          setGotWrong={setGotWrong}
+          userBadges={userBadges}
+          setUserBadges={setUserBadges}
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          modalBadge={modalBadge}
+          setModalBadge={setModalBadge}
+        />
+
+        <BadgeModal
+          isModalOpen={isModalOpen}
+          closeModal={closeModal}
+          modalBadge={modalBadge}
+        />
+      </div>
+    </>
+  );
+}
+
+export default Game;
