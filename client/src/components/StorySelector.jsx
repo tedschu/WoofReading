b88dabb0bb5e8f6ad559b6753c541a3d47fa47ab
tedschu <ted.schuster@gmail.com>
2024-08-27 11:51:44 -0500

    adding story generation

--- client/src/components/StorySelector.jsx
+++ client/src/components/StorySelector.jsx
@@ -4,58 +4,32 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import storyPrompts from "../utils/storyPrompts";
 
 export default function StorySelector({
   setStorySelection,
   setGotRight,
   setGotWrong,
 }) {
-  const [alignment, setAlignment] = useState("addition");
+  const [storyPrompt, setStoryPrompt] = useState("");
 
-  const handleChange = (event, newAlignment) => {
-    if (newAlignment != null) {
-      setAlignment(newAlignment);
-      setStorySelection(newAlignment);
-      setGotRight(false);
-      setGotWrong(false);
-    }
+  const selectPrompt = () => {
+    const randomIndex = Math.floor(Math.random() * storyPrompts.length);
+    const prompt = storyPrompts[randomIndex];
+    console.log(prompt);
+    setStoryPrompt(prompt);
   };
 
-  const buttonStyle = {
-    fontFamily: "Patrick Hand",
-    textTransform: "none",
+  const handleButton = () => {
+    selectPrompt();
   };
 
   return (
     <>
       <div className="gameSelectContainer">
-        <h3>What kind of story?</h3>
-
-        <ToggleButtonGroup
-          color="primary"
-          value={alignment}
-          exclusive
-          onChange={handleChange}
-          aria-label="Platform"
-          required
-          sx={{
-            "& .MuiToggleButton-root": {
-              ...buttonStyle,
-              color: "#0085bd",
-              border: "1px solid lightgray",
-              padding: "11px",
-              minWidth: "67px",
-            },
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-          }}
-        >
-          <ToggleButton value="goldendoodles">Goldendoodles</ToggleButton>
-          <ToggleButton value="pirates">Pirates</ToggleButton>
-          <ToggleButton value="outer space">Outer space</ToggleButton>
-          <ToggleButton value="Football">Football</ToggleButton>
-        </ToggleButtonGroup>
+        <h3>Let's do a story about:</h3>
+        <h3>{storyPrompt}</h3>
+        <button onClick={handleButton}>Try another one</button>
       </div>
     </>
   );
